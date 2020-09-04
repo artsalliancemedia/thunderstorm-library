daa900e5535b467fb524586fb8b31f827ef1a9f9
Jay Zhou <jayzml@gmail.com>
2020-09-04 16:49:18 +0800

    add http request utils

---
+++ thunderstorm/utils/requests_utils.py
@@ -0,0 +1,14 @@
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+retry_strategy = Retry(
+    total=3,
+    backoff_factor=1,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+http = Session()
+https = Session()
+https.mount("https://", adapter)
+http.mount("http://", adapter)
